8290e4ce12c04d1d02fc1984cf33552c93da7572
runic-tears <51153795+runic-tears@users.noreply.github.com>
2020-02-06 15:22:22 +0400

    Более гибкие интерфейсы

--- types/Stats.d.ts
+++ types/Stats.d.ts
@@ -1,5 +1,23 @@
+/*************************
+ * Типы для Annihilation *
+ *************************/
+
 export interface IAnnihilation {
-  global: {
+  global: IAnnihilationGlobal;
+  season: IAnnihilationSeason;
+}
+
+export interface IAnnihilationGlobal {
+  kills: number;
+  bowkills: number;
+  wood: number;
+  ores: number;
+  nexus: number;
+  digged: number;
+}
+
+export interface IAnnihilationSeason {
+  monthly: {
     kills: number;
     bowkills: number;
     wood: number;
@@ -7,105 +25,156 @@ export interface IAnnihilation {
     nexus: number;
     digged: number;
   };
-
-  season: {
-    monthly: {
-      kills: number;
-      bowkills: number;
-      wood: number;
-      ores: number;
-      nexus: number;
-      digged: number;
-    };
-  };
 }
 
+/************************
+ * Типы для BuildBattle *
+ ************************/
+
 export interface IBuildBattle {
-  global: {
+  global: IBuildBattleGlobal;
+  season: IBuildBattleSeason;
+}
+
+export interface IBuildBattleGlobal {
+  games: number;
+  wins: number;
+}
+
+export interface IBuildBattleSeason {
+  monthly: {
     games: number;
     wins: number;
   };
-
-  season: {
-    monthly: {
-      games: number;
-      wins: number;
-    };
-  };
 }
 
+/************************
+ * Типы для Block Party *
+ ************************/
+
 export interface IBlockParty {
-  global: {
+  global: IBlockPartyGlobal;
+  season: IBlockPartySeason;
+}
+
+export interface IBlockPartyGlobal {
+  games: number;
+  wins: number;
+  levels: number;
+}
+
+export interface IBlockPartySeason {
+  monthly: {
     games: number;
     wins: number;
     levels: number;
   };
-
-  season: {
-    monthly: {
-      games: number;
-      wins: number;
-      levels: number;
-    };
-  };
 }
 
+/*********************
+ * Типы для Bed Wars *
+ *********************/
+
 export interface IBedWars {
-  global: {
+  global: IBedWarsGlobal;
+  season: IBedWarsSeason;
+}
+
+export interface IBedWarsGlobal {
+  kills: number;
+  deaths: number;
+  games: number;
+  wins: number;
+  bedBreaked: number;
+}
+
+export interface IBedWarsSeason {
+  monthly: {
     kills: number;
     deaths: number;
     games: number;
     wins: number;
     bedBreaked: number;
   };
-
-  season: {
-    monthly: {
-      kills: number;
-      deaths: number;
-      games: number;
-      wins: number;
-      bedBreaked: number;
-    };
-  };
 }
 
+/************************
+ * Типы для Clash Point *
+ ************************/
+
 export interface IClashPoint {
-  global: {
+  global: IClashPointGlobal;
+  season: IClashPointSeason;
+}
+
+export interface IClashPointGlobal {
+  kills: number;
+  deaths: number;
+  games: number;
+  wins: number;
+  resourcePointsBreaked: number;
+}
+
+export interface IClashPointSeason {
+  monthly: {
     kills: number;
     deaths: number;
     games: number;
     wins: number;
     resourcePointsBreaked: number;
   };
-
-  season: {
-    monthly: {
-      kills: number;
-      deaths: number;
-      games: number;
-      wins: number;
-      resourcePointsBreaked: number;
-    };
-  };
 }
 
+/**********************
+ * Типы для Death Run *
+ **********************/
+
 export interface IDeathRun {
-  global: {
+  global: IDeathRunGlobal;
+  season: IDeathRunSeason;
+}
+
+export interface IDeathRunGlobal {
+  games: number;
+  wins: number;
+}
+
+export interface IDeathRunSeason {
+  monthly: {
     games: number;
     wins: number;
   };
-
-  season: {
-    monthly: {
-      games: number;
-      wins: number;
-    };
-  };
 }
 
+/*******************
+ * Типы для Дуэлей *
+ *******************/
+
 export interface IDuels {
-  global: {
+  global: IDuelsGlobal;
+  season: IDuelsSeason;
+}
+
+export interface IDuelsGlobal {
+  solo_wins: number;
+  solo_games: number;
+  team_wins: number;
+  team_games: number;
+  ranked_games: number;
+  ranked_wins: number;
+  total_wins: number;
+  total_games: number;
+  wins_classic: number;
+  wins_bow: number;
+  wins_op: number;
+  wins_potion: number;
+  wins_uhc: number;
+  wins_bwh: number;
+  maxstrike: number;
+}
+
+export interface IDuelsSeason {
+  monthly: {
     solo_wins: number;
     solo_games: number;
     team_wins: number;
@@ -120,100 +189,153 @@ export interface IDuels {
     wins_potion: number;
     wins_uhc: number;
     wins_bwh: number;
-    maxstrike: number;
-  };
-
-  season: {
-    monthly: {
-      solo_wins: number;
-      solo_games: number;
-      team_wins: number;
-      team_games: number;
-      ranked_games: number;
-      ranked_wins: number;
-      total_wins: number;
-      total_games: number;
-      wins_classic: number;
-      wins_bow: number;
-      wins_op: number;
-      wins_potion: number;
-      wins_uhc: number;
-      wins_bwh: number;
-      rate: number;
-      max_rate: number;
-    };
+    rate: number;
+    max_rate: number;
   };
 }
 
+/*********************
+ * Типы для Gun Game *
+ *********************/
+
 export interface IGunGame {
-  global: {
+  global: IGunGameGlobal;
+  season: IGunGameSeason;
+}
+
+export interface IGunGameGlobal {
+  kills: number;
+  wins: number;
+  games: number;
+  levels: number;
+}
+
+export interface IGunGameSeason {
+  monthly: {
     kills: number;
     wins: number;
     games: number;
     levels: number;
   };
-
-  season: {
-    monthly: {
-      kills: number;
-      wins: number;
-      games: number;
-      levels: number;
-    };
-  };
 }
 
+/*************************
+ * Типы для Hunger Games *
+ *************************/
+
 export interface IHungerGames {
-  global: {
+  global: IHungerGamesGlobal;
+  season: IHungerGamesSeason;
+}
+
+export interface IHungerGamesGlobal {
+  kills: number;
+  wins: number;
+  games: number;
+}
+
+export interface IHungerGamesSeason {
+  monthly: {
     kills: number;
     wins: number;
     games: number;
   };
-
-  season: {
-    monthly: {
-      kills: number;
-      wins: number;
-      games: number;
-    };
-  };
 }
+
+/********************
+ * Типы для Kit PVP *
+ ********************/
 
 export interface IKitPvp {
-  global: {
+  global: IKitPvpGlobal;
+  season: IKitPvpSeason;
+}
+
+export interface IKitPvpGlobal {
+  kills: number;
+  deaths: number;
+  points: number;
+}
+
+export interface IKitPvpSeason {
+  monthly: {
     kills: number;
     deaths: number;
-    points: number;
-  };
-
-  season: {
-    monthly: {
-      kills: number;
-      deaths: number;
-    };
   };
 }
 
+/*********************
+ * Типы для Mob Wars *
+ *********************/
+
 export interface IMobWars {
-  global: {
+  global: IMobWarsGlobal;
+  season: IMobWarsSeason;
+}
+
+export interface IMobWarsGlobal {
+  games: number;
+  wins: number;
+  mobsKilled: number;
+  mobsSended: number;
+  maxIncome: number;
+}
+
+export interface IMobWarsSeason {
+  monthly: {
     games: number;
     wins: number;
     mobsKilled: number;
     mobsSended: number;
-    maxIncome: number;
-  };
-  season: {
-    monthly: {
-      games: number;
-      wins: number;
-      mobsKilled: number;
-      mobsSended: number;
-    };
   };
 }
 
+/********************
+ * Типы для Призона *
+ ********************/
+
 export interface IPrison {
-  global: {
+  global: IPrisonGlobal;
+  season: IPrisonSeason;
+}
+
+export interface IPrisonGlobal {
+  total_blocks: number;
+  earned_money: number;
+  kills: number;
+  deaths: number;
+  mobs: number;
+  blocks: {
+    "1": number;
+    "3": number;
+    "12": number;
+    "13": number;
+    "14": number;
+    "15": number;
+    "16": number;
+    "17": number;
+    "22": number;
+    "3number": number;
+    "41": number;
+    "42": number;
+    "56": number;
+    "57": number;
+    "1number3": number;
+    "133": number;
+    "155": number;
+    "172": number;
+    "159:14": number;
+    "159:5": number;
+    "155:2": number;
+    "35:13": number;
+    "155:1": number;
+    "159:1": number;
+    "12:1": number;
+  };
+}
+
+export interface IPrisonSeason {
+  manual: {
     total_blocks: number;
     earned_money: number;
     kills: number;
@@ -227,51 +349,36 @@ export interface IPrison {
       "14": number;
       "15": number;
       "16": number;
-      "17": number;
-      "22": number;
-      "3number": number;
-      "41": number;
-      "42": number;
       "56": number;
-      "57": number;
-      "1number3": number;
-      "133": number;
-      "155": number;
-      "172": number;
-      "159:14": number;
-      "159:5": number;
       "155:2": number;
-      "35:13": number;
       "155:1": number;
-      "159:1": number;
-      "12:1": number;
-    };
-  };
-  season: {
-    manual: {
-      total_blocks: number;
-      earned_money: number;
-      kills: number;
-      deaths: number;
-      mobs: number;
-      blocks: {
-        "1": number;
-        "3": number;
-        "12": number;
-        "13": number;
-        "14": number;
-        "15": number;
-        "16": number;
-        "56": number;
-        "155:2": number;
-        "155:1": number;
-      };
     };
   };
 }
 
+/*********************
+ * Типы для Sky Wars *
+ *********************/
+
 export interface ISkyWars {
-  global: {
+  global: ISkyWarsGlobal;
+  season: ISkyWarsSeason;
+}
+
+export interface ISkyWarsGlobal {
+  wins: number;
+  games: number;
+  kills: number;
+  deaths: number;
+  arrowsFired: number;
+  blocksBroken: number;
+  blocksPlaced: number;
+  currentWinStreak: number;
+  winStreak: number;
+}
+
+export interface ISkyWarsSeason {
+  monthly: {
     wins: number;
     games: number;
     kills: number;
@@ -279,133 +386,189 @@ export interface ISkyWars {
     arrowsFired: number;
     blocksBroken: number;
     blocksPlaced: number;
-    currentWinStreak: number;
-    winStreak: number;
-  };
-
-  season: {
-    monthly: {
-      wins: number;
-      games: number;
-      kills: number;
-      deaths: number;
-      arrowsFired: number;
-      blocksBroken: number;
-      blocksPlaced: number;
-    };
   };
 }
+
+/******************
+ * Типы для Аркад *
+ ******************/
 
 export interface IArcade {
-  global: {
+  global: IArcadeGlobal;
+  season: IArcadeSeason;
+}
+
+export interface IArcadeGlobal {
+  games: number;
+  wins: number;
+  kills: number;
+  deaths: number;
+}
+
+export interface IArcadeSeason {
+  monthly: {
     games: number;
     wins: number;
     kills: number;
     deaths: number;
   };
-
-  season: {
-    monthly: {
-      games: number;
-      wins: number;
-      kills: number;
-      deaths: number;
-    };
-  };
 }
+
+/********************
+ * Типы для Мостов *
+ ********************/
 
 export interface IBridge {
-  global: {
-    games: number;
-    wins: number;
-    kills: number;
-    deaths: number;
-    points: number;
-  };
+  global: IBridgeGlobal;
 }
+
+export interface IBridgeGlobal {
+  games: number;
+  wins: number;
+  kills: number;
+  deaths: number;
+  points: number;
+}
+
+/************************
+ * Типы для Jump League *
+ ************************/
 
 export interface IJumpLeague {
-  global: {
-    games: number;
-    wins: number;
-    kills: number;
-    deaths: number;
-    checkpoints: number;
-  };
+  global: IJumpLeagueGlobal;
 }
+
+export interface IJumpLeagueGlobal {
+  games: number;
+  wins: number;
+  kills: number;
+  deaths: number;
+  checkpoints: number;
+}
+
+/***************************
+ * Типы для Murder Mystery *
+ ***************************/
 
 export interface IMurder {
-  global: {
-    games: number;
-    total_wins: number;
-    wins_as_innocent: number;
-    wins_as_maniac: number;
-    wins_as_detective: number;
-    kills: number;
-  };
+  global: IMurderGlobal;
 }
+
+export interface IMurderGlobal {
+  games: number;
+  total_wins: number;
+  wins_as_innocent: number;
+  wins_as_maniac: number;
+  wins_as_detective: number;
+  kills: number;
+}
+
+/**********************
+ * Типы для Пеинтбола *
+ **********************/
 
 export interface IPaintball {
-  global: {
-    games: number;
-    wins: number;
-    kills: number;
-    deaths: number;
-  };
+  global: IPaintballGlobal;
 }
+
+export interface IPaintballGlobal {
+  games: number;
+  wins: number;
+  kills: number;
+  deaths: number;
+}
+
+/***********************
+ * Типы для Sheep Wars *
+ ***********************/
 
 export interface ISheep {
-  global: {
-    games: number;
-    wins: number;
-    kills: number;
-    deaths: number;
-  };
+  global: ISheepGlobal;
 }
+
+export interface ISheepGlobal {
+  games: number;
+  wins: number;
+  kills: number;
+  deaths: number;
+}
+
+/**********************
+ * Типы для Turf Wars *
+ **********************/
 
 export interface ITurfWars {
-  global: {
-    games: number;
-    wins: number;
-    kills: number;
-    deaths: number;
-  };
+  global: ITurfWarsGlobal;
 }
 
+export interface ITurfWarsGlobal {
+  games: number;
+  wins: number;
+  kills: number;
+  deaths: number;
+}
+
+/*******************
+ * Типы для Сплифа *
+ *******************/
+
 export interface ISpleef {
-  global: {
-    games: number;
-    wins: number;
-    kills: number;
-    deaths: number;
-    broken_blocks: number;
-  };
+  global: ISpleefGlobal;
+}
+
+export interface ISpleefGlobal {
+  games: number;
+  wins: number;
+  kills: number;
+  deaths: number;
+  broken_blocks: number;
+}
+
+/*********************
+ * Типы для ТНТ Тега *
+ *********************/
+
+export interface ITntTag {
+  global: ITntTag;
 }
 
 export interface ITntTag {
-  global: {
-    games: number;
-    wins: number;
-    kills: number;
-  };
-}
-export interface ITntRun {
-  global: {
-    games: number;
-    wins: number;
-    broken_blocks: number;
-  };
+  games: number;
+  wins: number;
+  kills: number;
 }
 
-export interface ILuckyWars {
-  global: {
-    games: number;
-    wins: number;
-    kills: number;
-    deaths: number;
-    lucky_blocks: number;
-  };
+/*********************
+ * Типы для ТНТ Рана *
+ *********************/
+
+export interface ITntRun {
+  global: ITntRunGlobal;
 }
+
+export interface ITntRunGlobal {
+  games: number;
+  wins: number;
+  broken_blocks: number;
+}
+
+/***********************
+ * Типы для Lucky Wars *
+ ***********************/
+
+export interface ILuckyWars {
+  global: ILuckyWarsGlobal;
+}
+
+export interface ILuckyWarsGlobal {
+  games: number;
+  wins: number;
+  kills: number;
+  deaths: number;
+  lucky_blocks: number;
+}
+
+// Сгруппированная стата по модели из API
 
 export interface IUserStatsGroup {
   ANN: IAnnihilation;
